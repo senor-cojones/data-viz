f55404bde3e5bc63761e403217cab1e272e8a5be
Aaron <aaronmwhitecouk@googlemail.com>
2023-11-27 22:42:57 +0000

    Update app.js for server

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require("express");
-
 const app = express();
-
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log("Server started"));
+app.listen(port, () =>
+        console.log(`Server started. Listening on port ${port}`)
+);
 
-app.get('/backend', (req, res) => {
-  res.send({ express: "Express connected to React" });
+app.get("/backend", (req, res) => {
+        res.send({ express: "Express connected to React" });
 });
--- src/App.js
+++ src/App.js
@@ -3,37 +3,37 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-    state = {
-        data: null,
-    };
+        state = {
+                data: null,
+        };
 
-    componentDidMount() {
-        this.getData()
-            .then((res) => this.setState({ data: res.express }))
-            .catch((err) => console.log(err));
-    }
-
-    getData = async () => {
-        const response = await fetch("/backend");
-        const body = await response.json();
-
-        if (response.status !== 200) {
-            throw Error(body.message);
+        componentDidMount() {
+                this.getData()
+                        .then((res) => this.setState({ data: res.express }))
+                        .catch((err) => console.log(err));
         }
-        return body;
-    };
 
-    render() {
-        return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">{this.state.data}</p>
-            </div>
-        );
-    }
+        getData = async () => {
+                const response = await fetch("/backend");
+                const body = await response.json();
+
+                if (response.status !== 200) {
+                        throw Error(body.message);
+                }
+                return body;
+        };
+
+        render() {
+                return (
+                        <div className="App">
+                                <header className="App-header">
+                                        <img src={logo} className="App-logo" alt="logo" />
+                                        <h1 className="App-title">Welcome to React</h1>
+                                </header>
+                                <p className="App-intro">{this.state.data}</p>
+                        </div>
+                );
+        }
 }
 
 export default App;
